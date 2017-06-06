ed5591881efa977ebe3f30d1ed5af0bb63af992c
Ben Clayton <ben@calvium.com>
2017-06-06 09:55:06 +0100

    refactor to allow user to provide component name directly via parameter object. This means that names then survive minification

--- src/TestRegistry.js
+++ src/TestRegistry.js
@@ -5,14 +5,34 @@ import * as R from 'ramda';
  * Type of values in registeredScenes
  */
 export type RegisteredItemType = {|
-  name: string, // extracted from the component's name
+  name: string, // extracted from the component's name, or provided by user
   title: ?string, // title used when scene is used several times with different test data
   component: React$Element, // the actual element itself
   type: 'scene' | 'component',
   states?: Array<RegisteredItemType>,
 |};
 
+/**
+ * Parameter for the exported methods
+ */
+export type TestType = {
+  name: string,
+  title?: string,
+  wrapperStyle: ?Object,
+};
+
 const registeredItems: {|[string]: RegisteredItemType|} = {};
+
+/**
+ * Get the name of the component. Note only works in debug builds as production builds minify JS code and remove names
+ * Stateless components have .name, classes have .displayName.
+ */
+function getName(component: React$Element) {
+  if (!component) {
+    return '(no component)';
+  }
+  return component.type.displayName || component.type.name;
+}
 
 /**
  * Register a test item. Title is optional.
@@ -22,61 +42,56 @@ const registeredItems: {|[string]: RegisteredItemType|} = {};
  * @param title - subtitle for the test e.g. 'with long name'
  * @param type - For Screens/Scenes that should be displayed full-screen, use type='scene'. To display the same component in different states on the same screen, use type='component'.
  */
-function addTest(
-  component: React$Element,
-  title: ?string,
-  wrapperStyle: ?Object = {},
-  type: 'scene' | 'component' = 'scene'
-) {
+function addTest(component: React$Element, type: 'scene' | 'component', options: TestType = {}) {
   if (!component || !component.type) {
     return;
   }
-  // Get the name of the component.
-  // Stateless components have .name, classes have .displayName.
-  const componentName = component.type.displayName || component.type.name;
+  // Use provided name, or extract from component if not given
+  const name = options.name || getName(component);
+  const title = options.title;
 
-  const itemDetails = {name: componentName, component, title, wrapperStyle, type};
+  const itemDetails = {...options, component, type, name};
 
   switch (type) {
     // For each component, we store an array of different 'views' onto the component
     case 'component':
-      {
-        const key = componentName;
+    {
+      const key = name;
 
-        let existing: ?RegisteredItemType = registeredItems[key];
+      let existing: ?RegisteredItemType = registeredItems[key];
 
-        if (existing) {
-          if (!existing.states) {
-            console.log(`Probably trying to register a component on something previously registered as a scene`);
-            return;
-          }
-          existing.states = R.uniqBy(i => i.title, [...existing.states, itemDetails]); // remove dupes, based on title
-        } else {
-          // Register as empty placeholder so it can be rendered easily in the list
-          existing = {
-            component: null,
-            wrapperStyle: null,
-            name: componentName,
-            title: title,
-            type: 'component',
-            states: [itemDetails],
-          };
+      if (existing) {
+        if (!existing.states) {
+          console.log(`Probably trying to register a component on something previously registered as a scene`);
+          return;
         }
-
-        registeredItems[key] = existing;
-        // title is state1, state2, state3
-        existing.title = `${existing.states.length} test${existing.states.length !== 1 ? 's' : ''}`;
+        existing.states = R.uniqBy(i => i.title, [...existing.states, itemDetails]); // remove dupes, based on title
+      } else {
+        // Register as empty placeholder so it can be rendered easily in the list
+        existing = {
+          component: null,
+          wrapperStyle: null,
+          type: 'component',
+          states: [itemDetails],
+          title,
+          name,
+        };
       }
+
+      registeredItems[key] = existing;
+      // title is state1, state2, state3
+      existing.title = `${existing.states.length} test${existing.states.length !== 1 ? 's' : ''}`;
+    }
       break;
     // For scenes, we have a single item
     case 'scene':
-      {
-        const key = title || componentName;
-        if (registeredItems[key]) {
-          console.log(`Scene already registered with title=${key}. Overwriting..`);
-        }
-        registeredItems[key] = itemDetails;
+    {
+      const key = title || name;
+      if (registeredItems[key]) {
+        console.log(`Scene already registered with title=${key}. Overwriting..`);
       }
+      registeredItems[key] = itemDetails;
+    }
       break;
     default:
   }
@@ -92,14 +107,26 @@ function getTests(): Array<RegisteredItemType> {
 /**
  * Add test for a scene
  */
-const addSceneTest = (component: React$Element, title: ?string, wrapperStyle: ?Object = {}) =>
-  addTest(component, title, wrapperStyle, 'scene');
+const addSceneTest = (component: React$Element, titleOrOptions: ?string | ?TestType, wrapperStyle: ?Object = {}) => {
+  if (R.is(Object, titleOrOptions)) {
+    return addTest(component, 'scene', titleOrOptions);
+  }
+  addTest(component, 'scene', {title: titleOrOptions, wrapperStyle});
+};
 
 /**
  * Add test for a component
  */
-const addComponentTest = (component: React$Element, title: ?string, wrapperStyle: ?Object = {}) =>
-  addTest(component, title, wrapperStyle, 'component');
+const addComponentTest = (
+  component: React$Element,
+  titleOrOptions: ?string | ?TestType,
+  wrapperStyle: ?Object = {}
+) => {
+  if (R.is(Object, titleOrOptions)) {
+    return addTest(component, 'component', titleOrOptions);
+  }
+  addTest(component, 'component', {title: titleOrOptions, wrapperStyle});
+};
 
 /**
  * @deprecated - use addSceneTest
