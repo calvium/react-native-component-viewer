235779724a16c6a9174c747c8985779e3fa19e4a
Pablo Carrillo <pablo@calvium.com>
2017-06-09 10:44:45 +0100

    - Remove horizontal margins

--- src/DebugSceneList.js
+++ src/DebugSceneList.js
@@ -1,7 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 
 import {View, StyleSheet, Text, TouchableHighlight, Alert, ScrollView} from 'react-native';
-import {getTests} from './TestRegistry';
+import {getTests, TestType} from './TestRegistry';
 import type {RegisteredItemType} from './TestRegistry';
 import SearchableList from './SearchableList';
 
@@ -23,7 +23,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   componentWrapper: {
-    marginHorizontal: 20,
     alignSelf: 'stretch',
   },
   componentTitle: {
@@ -90,8 +89,8 @@ class DebugSceneList extends Component {
             return [
               <Text key={`${i.name}_${i.title}_title`} style={styles.componentTitle}>{i.title}</Text>,
               <View key={`${i.name}_${i.title}_component`} style={[styles.componentWrapper, i.wrapperStyle]}>
-              {i.component}
-            </View>]
+                {i.component}
+              </View>];
           })}
         </ScrollView>
       </View>
