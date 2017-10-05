a49b087b5581c9c4e62f0e5c8280d4cc927bc149
Richard Barrell <richard.barrell@calvium.com>
2017-10-05 16:05:38 +0100

    Fix PropTypes import (and bump version).

--- src/DebugSceneList.js
+++ src/DebugSceneList.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 import {View, StyleSheet, Text, TouchableHighlight, ScrollView, AsyncStorage} from 'react-native';
 import {getTests, getTest, addUpdateListener, removeUpdateListener} from './TestRegistry';
