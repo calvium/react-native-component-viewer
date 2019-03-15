c97ec5582a2b6fffa412ab2e395c442c8de32350
Pablo Carrillo <pablo.carrillo.alvarez@gmail.com>
2019-03-15 15:04:06 +0000

    add typescript definitions (#25)
    * Add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,35 @@
+declare module 'react-native-component-viewer' {
+  import {ReactNode, Component} from 'react';
+  import {ViewStyle} from 'react-native';
+
+  export interface ComponentViewerPropsType {
+    onClose: () => any;
+  }
+
+  // eslint-disable-next-line react/prefer-stateless-function
+  export class ComponentViewer extends Component<ComponentViewerPropsType> {}
+
+  /**
+   * Parameter for the exported methods
+   */
+  export interface TestInterface {
+    name: string;
+    title?: string;
+    wrapperStyle?: ViewStyle | null,
+  }
+
+  type TestFunctionType = (props: {closeThisTest: () => void}) => ReactNode<any>;
+  type TestType = TestFunctionType | ReactElement<any>;
+
+  function addComponentTest(
+    component: TestType,
+    options: string | TestInterface | null,
+    wrapperStyle?: ViewStyle | null,
+  ): void;
+
+  function addSceneTest(
+    component: TestType,
+    options: string | TestInterface | null,
+    wrapperStyle?: ViewStyle | null,
+  ): void;
+}
